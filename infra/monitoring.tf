4277f992a1901636b5e30985bcf016a98778c291
annamerheb <108671275+annamerheb@users.noreply.github.com>
2026-06-25 23:42:04 +0200

    fix: update Prometheus and Grafana volume paths for consistency

--- infra/monitoring.tf
+++ infra/monitoring.tf
@@ -18,8 +18,14 @@ resource "docker_container" "prometheus" {
   }
 
   volumes {
-    host_path      = abspath("${path.module}/../monitoring/prometheus.yml")
+    host_path      = "/var/jenkins_home/workspace/sentiment-ai-pipeline/monitoring/prometheus.yml"
     container_path = "/etc/prometheus/prometheus.yml"
+    read_only      = true
+  }
+
+  volumes {
+    host_path      = "/var/jenkins_home/workspace/sentiment-ai-pipeline/monitoring/alerts.yml"
+    container_path = "/etc/prometheus/alerts.yml"
     read_only      = true
   }
 }
